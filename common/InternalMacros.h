6cf52412d8d6cf1ef1a72ded35753844fa2146fd
shgli <li_shugan@126.com>
2014-01-19 00:29:11 +0800

    fix bug in CMakeLists.txt

--- common/InternalMacros.h
+++ common/InternalMacros.h
@@ -1,10 +1,14 @@
 #ifndef _INTERNAL_MACROS_H
 #define _INTERNAL_MACROS_H
 
+#ifdef _Win32
 #ifdef BUILDING_COMMON
 #define COMMON_EXPORT __declspec(dllexport)
 #else
 #define COMMON_EXPORT __declspec(dllimport) 
+#endif
+#else
+#define COMMON_EXPORT
 #endif
 #endif
 
--- log/InternalMacros.h
+++ log/InternalMacros.h
@@ -1,10 +1,14 @@
 #ifndef _LOG_INTERNAL_MACROS_
 #define _LOG_INTERNAL_MACROS_
 
+#ifdef _WIN32
 #ifdef BUILDING_LOG
 #define LOG_EXPORT __declspec(dllexport)
 #else
 #define LOG_EXPORT __declspec(dllimport)
+#endif
+#else
+#define LOG_EXPORT
 #endif
 #endif
 
